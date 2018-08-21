0339430afd477e2996c85641731f3a78876c91e1
tannyque <tannyque1992@gmail.com>
2018-08-21 19:35:44 +0100

    Changes hero constructor and adds a greet method

--- hero.js
+++ hero.js
@@ -1,9 +1,12 @@
-const Hero = function (name, health, favourite_food, talk, tasks) {
+const Hero = function (name, health, favourite_food) {
   this.name = name;
   this.health = health;
   this.favourite_food = favourite_food;
-  this.talk = talk;
-  this.tasks = tasks;
+  this.tasks = [];
 }
+
+Hero.prototype.greet = function () {
+  return 'Hi! My name is ' + this.name
+};
 
 module.exports = Hero;
