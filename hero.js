4c30d5c22edf52a949db56aa31c468433ddcf55c
tannyque <tannyque1992@gmail.com>
2018-08-21 19:00:29 +0100

    Creates hero constructor

---
+++ hero.js
@@ -0,0 +1,7 @@
+const Hero = function (name, health, favourite_food, talk, tasks) {
+  this.name = name;
+  this.health = health;
+  this.favourite_food = favourite_food;
+  this.talk = talk;
+  this.tasks = tasks;
+}
